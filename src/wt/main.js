1b819d689b2434c8acc8f7ea3f4c07f8b2d2e5d1
Oleksandr Sereda <sereda.san@gmail.com>
2023-06-29 12:23:40 +0300

    feag: make worker threads tasks

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,26 @@
+import os from "os";
+import { Worker } from "worker_threads";
+
+const WORKER_URL = new URL("worker.js", import.meta.url);
+
+const runWorker = (workerURL, workerData) => {
+  return new Promise((resolve, reject) => {
+    const worker = new Worker(workerURL, { workerData });
+    worker.on("message", (data) => resolve({ status: "resolved", data: data }));
+    worker.on("error", () => resolve({ status: "error", data: null }));
+  });
+};
+
 const performCalculations = async () => {
-    // Write your code here
+  const coresNumber = os.cpus().length;
+
+  const results = await Promise.all(
+    Array(coresNumber)
+      .fill()
+      .map((_, i) => runWorker(WORKER_URL, 10 + i))
+  );
+
+  console.log(results);
 };
 
 await performCalculations();
