a7a1113a167dd7fc0defb2f32471db9070b8177f
Oleksandr Sereda <sereda.san@gmail.com>
2023-06-29 10:03:34 +0300

    feat: make fs tasks

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,15 @@
+import fs from "fs/promises";
+
+const FILENAME = "files/fresh.txt";
+const TEXT = "I am fresh and young";
+
 const create = async () => {
-    // Write your code here 
+  try {
+    const filename = new URL(FILENAME, import.meta.url).pathname;
+    await fs.writeFile(filename, TEXT, { encoding: "utf8", flag: "wx" });
+  } catch (error) {
+    throw error.code === "EEXIST" ? new Error("FS operation failed") : error;
+  }
 };
 
 await create();
