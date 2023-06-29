62840d230021833352f7cb74294d3693792eb4c5
Oleksandr Sereda <sereda.san@gmail.com>
2023-06-29 11:42:38 +0300

    feat: make zip tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import { createReadStream, createWriteStream } from "fs";
+import { pipeline } from "stream/promises";
+import { createGzip } from "zlib";
+
+const FILE = "files/fileToCompress.txt";
+const ARCH = "files/archive.gz";
+
 const compress = async () => {
-    // Write your code here 
+  const gzip = createGzip();
+  const source = createReadStream(new URL(FILE, import.meta.url).pathname);
+  const destination = createWriteStream(
+    new URL(ARCH, import.meta.url).pathname
+  );
+
+  await pipeline(source, gzip, destination);
 };
 
 await compress();
