7f7dc58d0f1919b6e3f934e1f3873ca273a45575
Oleksandr Sereda <sereda.san@gmail.com>
2023-06-29 10:28:10 +0300

    feat: make cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,21 @@
 const parseArgs = () => {
-    // Write your code here 
+  const kvArgs = {};
+  let lastKey = null;
+  process.argv.slice(2).forEach((arg) => {
+    if (arg.startsWith("--")) {
+      lastKey = arg.slice(2);
+      kvArgs[lastKey] = "";
+    } else if (lastKey) {
+      kvArgs[lastKey] = arg;
+      lastKey = null;
+    }
+  });
+
+  console.log(
+    Object.entries(kvArgs)
+      .map(([key, val]) => `${key} is ${val}`)
+      .join(", ")
+  );
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+  const rssVars = Object.entries(process.env).reduce((acc, [key, val]) => {
+    if (key.startsWith("RSS_")) {
+      acc.push(`${key}=${val}`);
+    }
+    return acc;
+  }, []);
+  console.log(rssVars.join("; "));
 };
 
 parseEnv();
